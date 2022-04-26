b43e2e1e621c0ce595e4a8315d6ca6c19e029c84
Liav <oneandonlyliav@gmail.com>
2022-04-26 18:47:43 -0400

    converted Educational to class component

--- src/components/Educational.js
+++ src/components/Educational.js
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Educational() {
-  return (
-    <div className="educational">
-      <form className="educational-form">
-        <label>
-          School Name:
-          <input type="text" />
-        </label>
-        <label>
-          Title of Study:
-          <input type="text" />
-        </label>
-        <label>
-          Date of Study:
-          <input type="text" />
-        </label>
-      </form>
-    </div>
-  );
+class Educational extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      school: '',
+      title: '',
+      date: ''
+    };
+
+    this.handleSchoolChange = this.handleSchoolChange.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+  }
+
+  handleSchoolChange(e) {
+    this.setState({ school: e.target.value });
+  }
+
+  handleTitleChange(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  handleDateChange(e) {
+    this.setState({ date: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="educational">
+        <form className="educational-form" onSubmit={this.handleSubmit}>
+          <label>
+            School Name:
+            <input type="text" onChange={this.handleSchoolChange} />
+          </label>
+          <label>
+            Title of Study:
+            <input type="text" onChange={this.handleTitleChange} />
+          </label>
+          <label>
+            Date of Study:
+            <input type="text" onChange={this.handleDateChange} />
+          </label>
+          <input type="submit" value="Submit" />
+          <button type="button">Edit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Educational;

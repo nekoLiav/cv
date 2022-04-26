bc9fc99e9511c6d1751af0a829335b6b019f45e3
Liav <oneandonlyliav@gmail.com>
2022-04-26 18:48:16 -0400

    moved edit buttons into components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import General from './components/General';
 import Educational from './components/Educational';
 import Practical from './components/Practical';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hello: 'hello'
-    };
-  }
-
-  render() {
-    const { hello } = this.state;
-    return (
-      <div className="main">
-        <General hello={hello} />
-        <button type="button">Edit</button>
-        <Educational />
-        <button type="button">Edit</button>
-        <Practical />
-        <button type="button">Edit</button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="main">
+      <General />
+      <Educational />
+      <Practical />
+    </div>
+  );
 }
 
 export default App;

b0658d320ca48790921727ecfb57423b49ccd516
Liav <oneandonlyliav@gmail.com>
2022-04-26 18:29:57 -0400

    converted General to class component

--- src/components/General.js
+++ src/components/General.js
@@ -1,36 +1,73 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-function General(props) {
-  const { hello } = props;
+class General extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="general">
-      <form className="general-form">
-        <label htmlFor="name">
-          Name:
-          <input type="text" id="name" />
-        </label>
-        <label htmlFor="email">
-          Email:
-          <input type="email" id="email" />
-        </label>
-        <label htmlFor="phone">
-          Phone Number:
-          <input type="text" id="phone" />
-        </label>
-      </form>
-      <p>{hello}</p>
-    </div>
-  );
+    this.state = {
+      name: '',
+      email: '',
+      phone: ''
+    };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  handlePhoneChange(e) {
+    this.setState({ phone: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="general">
+        <form className="general-form" onSubmit={this.handleSubmit}>
+          <label htmlFor="name">
+            Name:
+            <input
+              type="text"
+              id="name"
+              value={this.state.value}
+              onChange={this.handleNameChange}
+            />
+          </label>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              id="email"
+              value={this.state.value}
+              onChange={this.handleEmailChange}
+            />
+          </label>
+          <label htmlFor="phone">
+            Phone Number:
+            <input
+              type="text"
+              id="phone"
+              value={this.state.value}
+              onChange={this.handlePhoneChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+          <button type="button">Edit</button>
+        </form>
+      </div>
+    );
+  }
 }
-
-General.propTypes = {
-  hello: PropTypes.string
-};
-
-General.defaultProps = {
-  hello: 'john'
-};
 
 export default General;

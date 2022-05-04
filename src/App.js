80f8cc7e375a0034adbad72851d1cea7ed84127e
Liav <oneandonlyliav@gmail.com>
2022-05-03 21:27:55 -0400

    App.js refactored using hooks

--- src/App.js
+++ src/App.js
@@ -1,88 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import General from './components/General';
 import Educational from './components/Educational';
 import Practical from './components/Practical';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [mode, setMode] = useState(true);
 
-    this.state = {
-      mode: true,
-      toggleButtonText: 'Preview Mode',
-      educationList: [{ key: Date.now() }],
-      practicalList: [{ key: Date.now() }]
-    };
+  const [educationList, setEducationList] = useState([{ key: Date.now() }]);
 
-    this.addEducation = this.addEducation.bind(this);
-    this.addEmployment = this.addEmployment.bind(this);
-    this.toggleMode = this.toggleMode.bind(this);
-  }
+  const [employmentList, setEmploymentList] = useState([{ key: Date.now() }]);
 
-  addEducation() {
-    this.setState((prevState) => ({
-      educationList: [...prevState.educationList, { key: Date.now() }]
-    }));
-  }
-
-  addEmployment() {
-    this.setState((prevState) => ({
-      practicalList: [...prevState.practicalList, { key: Date.now() }]
-    }));
-  }
-
-  toggleMode() {
-    this.setState(
-      this.state.mode
-        ? { toggleButtonText: 'Edit Mode', mode: false }
-        : { toggleButtonText: 'Preview Mode', mode: true }
-    );
-  }
-
-  render() {
-    return (
-      <div className="page">
-        <header>
-          <p>Simple CV</p>
-        </header>
-        <div className="main">
-          <div className="candidate-info">
-            <General mode={this.state.mode} />
-            <div className="info-containers">
-              <div className="educational-container">
-                <h1>Education Background</h1>
-                {this.state.educationList.map((l) => (
-                  <Educational key={l.key} mode={this.state.mode} />
-                ))}
-                {this.state.mode && (
-                  <button type="button" className="add-button" onClick={this.addEducation}>
-                    Add Education
-                  </button>
-                )}
-              </div>
-              <div className="practical-container">
-                <h1>Employment Background</h1>
-                {this.state.practicalList.map((l) => (
-                  <Practical key={l.key} mode={this.state.mode} />
-                ))}
-                {this.state.mode && (
-                  <button type="button" className="add-button" onClick={this.addEmployment}>
-                    Add Employment
-                  </button>
-                )}
-              </div>
+  return (
+    <div className="page">
+      <header>
+        <p>Simple CV</p>
+      </header>
+      <div className="main">
+        <div className="candidate-info">
+          <General mode={mode} />
+          <div className="info-containers">
+            <div className="educational-container">
+              <h1>Education Background</h1>
+              {educationList.map((l) => (
+                <Educational key={l.key} mode={mode} />
+              ))}
+              {mode && (
+                <button
+                  type="button"
+                  className="add-button"
+                  onClick={() =>
+                    setEducationList((prevState) => [...prevState, { key: Date.now() }])
+                  }>
+                  Add Education
+                </button>
+              )}
+            </div>
+            <div className="practical-container">
+              <h1>Employment Background</h1>
+              {employmentList.map((l) => (
+                <Practical key={l.key} mode={mode} />
+              ))}
+              {mode && (
+                <button
+                  type="button"
+                  className="add-button"
+                  onClick={() =>
+                    setEmploymentList((prevState) => [...prevState, { key: Date.now() }])
+                  }>
+                  Add Employment
+                </button>
+              )}
             </div>
           </div>
         </div>
-        <div className="button-container">
-          <button type="button" className="toggle-edit-button" onClick={this.toggleMode}>
-            {this.state.toggleButtonText}
-          </button>
-        </div>
-        <footer></footer>
       </div>
-    );
-  }
-}
+      <div className="button-container">
+        <button
+          type="button"
+          className="toggle-edit-button"
+          onClick={() => setMode(mode ? false : true)}>
+          {mode ? 'Preview Mode' : 'Edit Mode'}
+        </button>
+      </div>
+      <footer></footer>
+    </div>
+  );
+};
 
 export default App;
